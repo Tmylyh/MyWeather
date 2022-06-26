f02370d30a0de47fdef836ba31faa95a2b2e759e
mac <mac@macdeMacBook-Pro.local>
2022-06-26 23:36:44 +0800

    Add gitignore

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by mac on 2022/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

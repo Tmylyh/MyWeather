4641cd9619c6b53d6d9e8cf30bbaf0d154ae67e8
mac <mac@macdeMacBook-Pro.local>
2022-06-27 03:17:46 +0800

    Add Alamofire SwiftyJSON CLLocation

--- MyWeather/ViewController.swift
+++ MyWeather/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+let frame = UIScreen.main.bounds
+let ScreenWidth = frame.width
+let ScreenHeight = frame.height
 
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    let locationManager = CLLocationManager()
+    
+    lazy var backgroundImage: UIImageView = {
+        let backgroundImage = UIImageView(frame: frame)
+        backgroundImage.contentMode = .scaleAspectFill
+        backgroundImage.image = UIImage(named: "bg")
+        return backgroundImage
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupView()
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager.requestLocation()
     }
-
-
+    
+    func setupView(){
+        self.view.addSubview(backgroundImage)
+        self.view.sendSubviewToBack(backgroundImage)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
 }
-
